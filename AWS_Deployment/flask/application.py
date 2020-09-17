bf652db5b570b6a740d05b091b40e611a84b209c
andre <awjrs22@gmail.com>
2020-09-16 20:43:42 -0400

    updates with firebase creds

--- AWS_Deployment/flask/application.py
+++ AWS_Deployment/flask/application.py
@@ -1,18 +1,53 @@
 # Make a flask API for our DL Model
 
+from flask_restplus import Api, Resource, fields
 # Import the WSGI application library
+from werkzeug.utils import cached_property
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 
 # Import FLASK
 from flask import Flask, request, jsonify
 
-# Import RESTPLUS libraries
-from flask_restplus import Api, Resource, fields
-
-# Import DS libraries
 import numpy as np
 import pickle
+
+from firebase import firebase
+
+import pyrebase
+
+config = {
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "storageBucket": ""
+}
+
+
+firebase = pyrebase.initialize_app(config)
+userEmail = ''
+userPass = ''
+
+auth = firebase.auth()
+
+# Log the user in
+user = auth.sign_in_with_email_and_password(userEmail, userPass)
+
+# Get a reference to the database service
+db = firebase.database()
+
+# data to save
+data = {
+            "name": "Mortimer Williams"
+            }
+
+# Pass the user's idToken to the push method
+results = db.child("test").push(data, user['idToken'])
+print results
+results = db.child("test").get(user['idToken'])
+print results.val()
+
+
 
 # Define app and api object
 application = Flask(__name__)
@@ -28,6 +63,7 @@ labelEncoder1 = pickle.load(open('pickle_files/labelEncoder1.pickle', 'rb'))
 labelEncoder2 = pickle.load(open('pickle_files/labelEncoder2.pickle', 'rb'))
 standardScaler = pickle.load(open('pickle_files/standardScaler.pickle', 'rb'))
 model = pickle.load(open('pickle_files/log_reg_model.pickle', 'rb'))
+
 
 
 @ns.route('/prediction')
